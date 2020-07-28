af52ba5d9f920d6a2225e097c1c7fdcaddb27de7
Sergei <sergokondrashevskij@ukr.net>
2020-07-28 15:35:09 +0300

    Lesson №2

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,11 @@
 import { radioPlayerInit } from './radioPlayer.js'
+import { musicPlayerInit } from './musicPlayer.js'
+import { videoPlayerInit } from './videoPlayer.js'
 
 const playerBtn = document.querySelectorAll('.player-btn')
 const playerBlock = document.querySelectorAll('.player-block')
 const temp = document.querySelector('.temp')
+
 
 const deactivetionPlayer = () => {
 	temp.style.display = 'none'
@@ -18,4 +21,6 @@ playerBtn.forEach((btn, i) =>
 	})
 )
 
+videoPlayerInit()
+musicPlayerInit()
 radioPlayerInit()
